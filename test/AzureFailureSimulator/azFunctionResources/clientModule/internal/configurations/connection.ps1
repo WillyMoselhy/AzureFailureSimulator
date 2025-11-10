f91453e0448dc7bc89f2153ca7f255e8d12783a4
Willy Moselhy [MSFT] <willy@moselhy.com>
2025-11-10 13:38:51 +0400

    Adding GitHub Actions

---
+++ test/AzureFailureSimulator/azFunctionResources/clientModule/internal/configurations/connection.ps1
@@ -0,0 +1,3 @@
+﻿Set-PSFConfig -Module 'AzureFailureSimulator' -Name 'Client.Uri' -Value $null -Initialize -Validation 'string' -Description "Url to connect to the AzureFailureSimulator Azure function"
+Set-PSFConfig -Module 'AzureFailureSimulator' -Name 'Client.UnprotectedToken' -Value '' -Initialize -Validation 'string' -Description "The unencrypted access token to the AzureFailureSimulator Azure function. ONLY use this from secure locations or non-sensitive functions!"
+Set-PSFConfig -Module 'AzureFailureSimulator' -Name 'Client.ProtectedToken' -Value $null -Initialize -Validation 'credential' -Description "An encrypted access token to the AzureFailureSimulator Azure function. Use this to persist an access token in a way only the current user on the current system can access."
---
+++ test/AzureFailureSimulator/library/AzureFailureSimulator/AzureFailureSimulator/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AzureFailureSimulator
+{
+    public class Class1
+    {
+    }
+}
